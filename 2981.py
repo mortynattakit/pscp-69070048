5f25cb9c3cfcd227574cdeee0f4508833d94990f
Suw1 <69070048@kmitl.ac.th>
2026-07-03 14:44:24 +0700

    Update 2981.py

--- 2981.py
+++ 2981.py
@@ -1,5 +1,6 @@
 """This code says hello to the name as the the user inputs"""
 firstname = input()
 lastname = input()
+#prints the output
 print(f"Hello {firstname} {lastname}")
-print(firstname[0:2]+lastname[0:2])
+print(f"{firstname[0:2]} {lastname[0:2]}")
